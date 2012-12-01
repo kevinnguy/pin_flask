ee8782a43c817c4173c5f565a6385690dce73be2
Kevin Nguy <kevnguy@gmail.com>
2012-12-01 14:50:41 -0800

    reverted back to simple app

--- app/views.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import Flask, render_template, request, redirect, url_for
-
-@app.route('/')
-@app.route('/home')
-def index():
-    return render_template('home.html')
